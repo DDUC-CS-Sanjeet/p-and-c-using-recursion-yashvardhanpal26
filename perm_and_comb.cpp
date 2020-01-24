93225ecc48cbf043fbd9c2b430c0f5a6ded340ea
yashvardhanpal <yashvardhanpal26@gmail.com>
2020-01-24 16:19:48 +0530

    2nd commit

--- perm_and_comb.cpp
+++ perm_and_comb.cpp
@@ -1,17 +1,89 @@
+
+
 #include<iostream>
+
 using namespace std;
 
-int permutation(int n, int r)
+
+
+int permutation(float n, float r)
+
 {
-  return 0;
+    if(r==0)
+    {
+        return 1;
+
+    }
+    else
+    {
+      return (n)*permutation(n-1,r-1);
+    }
+
+
 }
 
-int combination(int n, int r)
+
+
+
+float combination(float n, float r)
+
 {
-  return 0;
+
+      if(r==0)
+    {
+        return 1;
+
+    }
+    else
+    {
+      return (n/r)*combination(n-1,r-1);
+    }
+
+
+
 }
+
+
+
 
 int main()
+
 {
-  return 0;
+    int x=1;
+    float p,c,n,r;
+    cout<<"enter value of n and r \n";
+    cin>>n>>r;
+    try
+    {
+           if(r==0)
+          {
+             p=1;
+            c=1;
+     	  }
+     	  else if(n<r)
+     	  {
+     	  	throw x;
+		   }
+       	  else if(n<0||r<0)
+          {
+              throw x;
+          }
+          else 
+           { 
+            p=permutation(n,r);
+            c=combination(n,r);
+
+           }
+    cout<<"\n permutation = "<<p;
+    
+	cout<<"\n combination = "<<c;
+
+   }
+   catch(int a)
+   {
+       cout<<"\ninvalid";                
+        
+    }
+      return 0;
+
 }
